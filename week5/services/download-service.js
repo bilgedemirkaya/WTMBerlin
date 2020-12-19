59ec9f22e8b1a778c8c7aeeec81345794bbbfebc
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-12-19 17:23:04 +0300

    Add week5 homework

--- week5/services/download-service.js
+++ week5/services/download-service.js
@@ -1,10 +1,7 @@
 const BaseService = require('./base-service')
-const PhoneModel = require('../models/phone')
 const platformService = require('../services/platform-service')
 
 class DownloadService extends BaseService {
-    model = PhoneModel
-
     async downloadApp(phone, app){
         const socialApp = await platformService.find(app)
         phone.apps.push(app)
